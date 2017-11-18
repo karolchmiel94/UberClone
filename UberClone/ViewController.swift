ae71285bc219cdbce7eb7d2d24abfb07300cf6e5
karol <karolch94>
2017-11-18 19:44:59 +0100

    Initial Commit

---
+++ UberClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UberClone
+//
+//  Created by Karol Chmiel on 18/11/2017.
+//  Copyright © 2017 Karol Chmiel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
